61e4457fd12e2ab352c02dc54dc84fde94fb58c4
Julia <juliabomfimdeoliveira@hotmail.com>
2021-10-20 17:51:11 -0300

    Projeto finalizado

--- src/components/Theme/index.tsx
+++ src/components/Theme/index.tsx
@@ -1,6 +1,8 @@
-import { ReactNode } from 'react'
-import { Header } from '../Header'
-import * as C from './styles'
+import { ReactNode } from 'react';
+import { Header } from '../Header';
+import { SidebarItem } from '../SidebarItem';
+import * as C from './styles';
+import { useForm } from '../../contexts/FormContext'
 
 
 type Props = {
@@ -8,6 +10,8 @@ type Props = {
 }
 
 export const Theme = ({ children }: Props) => {
+    const { state } = useForm();
+
     return (
         <C.Container>
             <C.Area>
@@ -15,7 +19,31 @@ export const Theme = ({ children }: Props) => {
 
                 <C.Steps>
                     <C.Sidebar>
-                        ...
+                        
+                        <SidebarItem 
+                            title='Pessoal'
+                            description='Se identifique'
+                            icon='profile'
+                            path='/'
+                            active={state.currentStep === 1}
+                        />
+
+                        <SidebarItem 
+                            title='Profissional'
+                            description='Seu nível'
+                            icon='book'
+                            path='/step2'
+                            active={state.currentStep === 2}
+                        />
+
+                        <SidebarItem 
+                            title='Contatos'
+                            description='Como te achar'
+                            icon='mail'
+                            path='/step3'
+                            active={state.currentStep === 3}
+                        />
+
                     </C.Sidebar>
                     <C.Page>
                         {children}
